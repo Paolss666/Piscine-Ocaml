be3c87ef6516f42a4b9bb2812e2f13f38e13e4f9
Nicola PAOLETTI <npaolett@f2r8s6.clusters.42paris.fr>
2026-01-06 14:49:57 +0100

    fibonacci

---
+++ Recursion-and-higher-order-functions/ex04/src/fibonacci.ml
@@ -0,0 +1,16 @@
+let rec fibonacci n =
+  if n < 0 then
+   -1
+  else if n = 0 || n = 1 then
+    n
+  else
+    fibonacci(n - 2) + fibonacci(n -1)
+
+let () =
+  print_int (fibonacci (-42));
+  print_char '\n';
+    print_int (fibonacci 1);
+  print_char '\n';
+    print_int (fibonacci 3);
+  print_char '\n';
+    print_int (fibonacci 6);
