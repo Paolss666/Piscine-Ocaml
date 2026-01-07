8473b08498a115970e94f200ad9365b89d369ba6
Nicola PAOLETTI <npaolett@f2r8s6.clusters.42paris.fr>
2026-01-07 11:22:11 +0100

    crossover

---
+++ Pattern-Matching-and-Data-Types/ex01/src/crossover.ml
@@ -0,0 +1,41 @@
+let rec is_in elem lst = 
+  match lst with
+  | [] -> false
+  | x :: xs ->
+    if x = elem then true
+    else is_in elem xs
+
+
+let rec crossover lst lst1 = 
+  match lst with
+  | [] -> []
+  | x :: xs ->
+    if is_in x lst1 then x :: crossover xs lst1
+    else crossover xs lst1
+
+
+let rec print_list lst =
+  match lst with
+  | [] -> print_newline ()
+  | x :: resto ->
+      print_int x;
+      print_string " ";
+      print_list resto
+
+let () =
+  print_endline "Test 1:";
+  let result1 = crossover [1; 2; 3; 4] [3; 4; 5; 6] in
+  print_list result1;
+  
+  print_endline "Test 2:";
+  let result2 = crossover [1; 2] [3; 4] in
+  print_list result2;  
+  
+  print_endline "Test 3:";
+  let result3 = crossover [] [1; 2; 3] in
+  print_list result3;  
+  
+  print_endline "Test 4:";
+  let result4 = crossover ['a'; 'b'; 'c'] ['b'; 'c'; 'd'] in
+  List.iter (fun c -> Printf.printf "%c " c) result4;
+  print_newline ()  (* → b c *)
