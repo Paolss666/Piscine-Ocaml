51d2d476cf2cd77f1caa96960209c8c81efff9f6
Nicola PAOLETTI <npaolett@f4r6s3.clusters.42paris.fr>
2026-01-26 12:42:24 +0100

    sum ref

---
+++ Imperative/ex04/src/sum.ml
@@ -0,0 +1,17 @@
+let sum ( n: float) ( m: float) : float =
+  let result = ref 0.0 in 
+  let i = ref n in 
+  let j = ref m in
+  result := !i +. !j;
+  !result
+
+let () =
+  let n = 1.0 in
+  let m = 5.4 in
+  let result1 = sum n m in
+  let l = 4.3 in
+  let p = 2.2 in
+  let result = sum l p in
+  Printf.printf "The sum from %.1f to %.1f is %.1f\n" l p result;
+  Printf.printf "The sum from %.1f to %.1f is %.1f\n" n m result1;
+  Printf.printf "The sum from %.1f to %.1f is %.1f\n" result result1 (sum result result1)
