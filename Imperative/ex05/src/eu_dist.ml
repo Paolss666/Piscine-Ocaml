2e9eb54acc97af329e66a9927d0f2ca0de9bb312
Nicola PAOLETTI <npaolett@f4r6s3.clusters.42paris.fr>
2026-01-26 13:04:43 +0100

    euclide distance

---
+++ Imperative/ex05/src/eu_dist.ml
@@ -0,0 +1,14 @@
+
+let eu_dist (arr : float array) (arr1 : float array) : float =
+  let sum_s = ref 0.0 in 
+  for i = 0 to (Array.length arr -1) do
+    let dif = arr.(i) -. arr1.(i) in
+    sum_s := !sum_s +. (dif *. dif)
+  done;
+  sqrt !sum_s
+
+let () =
+  let point1 = [|1.0; 2.0; 3.2|] in
+  let point2 = [|4.0; 6.0; 5.0|] in
+  let distance = eu_dist point1 point2 in
+  Printf.printf "Euclidean distance: %f\n" distance
