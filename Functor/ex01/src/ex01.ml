d38b26a2fdfb1866e1b027d644ae439bd5f3ca86
Nicola PAOLETTI <npaolett@f4r2s1.clusters.42paris.fr>
2026-01-27 15:12:58 +0100

    add ex using Hashtbl build

--- Functor/ex01/src/ex01.ml
+++ Functor/ex01/src/ex01.ml
@@ -12,13 +12,11 @@ let pairs = List.map (fun s -> (s, String.length s)) values in
 List.iter (fun (k,v) -> StringHashtbl.add ht k v) pairs;
 StringHashtbl.iter (fun k v -> Printf.printf "k = \"%s\", v = %d\n" k v) ht
 
-
-
 let ( ) =
-
+Printf.printf "\nUsing built-in Hashtbl:\n";
 let ht = Hashtbl.create 5 in
 let values = [ "Hello"; "world"; "42"; "Ocaml"; "H" ] in
 let pairs = List.map (fun s -> (s, String.length s))
   values in
 List.iter (fun (k,v) -> Hashtbl.add ht k v) pairs;
-Hashtbl.iter (fun k v -> Printf.printf "k = \"%s\", v = %d\n" k v) ht 
+Hashtbl.iter (fun k v -> Printf.printf "k = \"%s\", v = %d\n" k v) ht
