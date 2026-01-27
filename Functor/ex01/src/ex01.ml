66831e2d086d02343f9aca461a14c3bfdf220f16
Nicola PAOLETTI <npaolett@f6r10s9.clusters.42paris.fr>
2026-01-27 13:32:44 +0100

    done ex01

--- Functor/ex01/src/ex01.ml
+++ Functor/ex01/src/ex01.ml
@@ -11,3 +11,14 @@ let values = [ "Hello"; "world"; "42"; "Ocaml"; "H" ] in
 let pairs = List.map (fun s -> (s, String.length s)) values in
 List.iter (fun (k,v) -> StringHashtbl.add ht k v) pairs;
 StringHashtbl.iter (fun k v -> Printf.printf "k = \"%s\", v = %d\n" k v) ht
+
+
+
+let ( ) =
+
+let ht = Hashtbl.create 5 in
+let values = [ "Hello"; "world"; "42"; "Ocaml"; "H" ] in
+let pairs = List.map (fun s -> (s, String.length s))
+  values in
+List.iter (fun (k,v) -> Hashtbl.add ht k v) pairs;
+Hashtbl.iter (fun k v -> Printf.printf "k = \"%s\", v = %d\n" k v) ht 
