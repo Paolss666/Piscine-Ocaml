50a99d72bf62d8ec1565c4423e55b282a8d92f98
Nicola PAOLETTI <npaolett@f4r6s8.clusters.42paris.fr>
2026-01-28 14:56:21 +0100

    fix repeat_string

--- Recursion-and-higher-order-functions/ex01/src/repeat_string.ml
+++ Recursion-and-higher-order-functions/ex01/src/repeat_string.ml
@@ -1,17 +1,26 @@
-let repeat_string s n = 
+let repeat_string ?(s="") n = 
   if n < 0 then
-    "Error"
-  else
-    let rec loop i acc =
+      "Error"
+  else if s = "" then
+    let rec loop i acc = 
       if i >= n then
         acc
       else
-        loop (i + 1) (acc ^ s)
+        loop (i + 1) (acc ^ "x")
     in
     loop 0 ""
+  else
+      let rec loop i acc =
+        if i >= n then
+          acc
+        else
+          loop (i + 1) (acc ^ s)
+      in
+      loop 0 ""
 
 let () =
- print_endline(repeat_string "lol" 3);
- print_endline(repeat_string "lol" (-1));
- print_endline(repeat_string "" 0);
- print_endline(repeat_string "WTF" 10);
+ print_endline(repeat_string  ~s:"toto" 3);
+ print_endline(repeat_string ~s:"lol" (-1));
+ print_endline(repeat_string 0);
+ print_endline(repeat_string ~s:"WTF" 10);
+print_endline(repeat_string  3);
