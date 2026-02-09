bb3d9c84741e546353529243df68ec3ba0aff7fc
Nicola PAOLETTI <npaolett@f4r3s8.clusters.42paris.fr>
2026-02-09 10:20:14 +0100

    fix encode.ml

--- Pattern-Matching-and-Data-Types/ex00/src/encode.ml
+++ Pattern-Matching-and-Data-Types/ex00/src/encode.ml
@@ -1,36 +1,51 @@
-let rec count_elem elem list = 
+let string_to_list s : char list =
+  let rec aux i acc =
+    if i < 0 then acc
+    else aux (i - 1) (s.[i] :: acc)
+  in
+  aux (String.length s - 1) []
+
+
+let rec count_consecutive elem list = 
   match list with
   | [] -> 0
   | x :: xs ->
-    if x = elem then 1 + count_elem elem xs
-    else 0
+      if x = elem then 1 + count_consecutive elem xs
+      else 0
 
 let rec skip n list = 
   match list with
   | [] -> []
   | x :: xs ->
-    if n <= 0 then list
-    else skip (n -1) xs
-
-
+      if n <= 0 then list
+      else skip (n - 1) xs
 
 let rec encode list = 
   match list with
   | [] -> []
   | x :: xs ->
-    let count = 1 + count_elem x xs in
-    let list_skipped = skip count list in
-    (count, x) :: encode list_skipped
+      let count = 1 + count_consecutive x xs in
+      let list_skipped = skip count list in
+      (count, x) :: encode list_skipped
 
 
 let rec print_result lst =
   match lst with
   | [] -> print_newline ()
-  | (num, lettera) :: resto ->
-      Printf.printf "(%d, '%c') " num lettera;
-      print_result resto
+  | (num, c) :: suiv ->
+      Printf.printf "(%d, '%c') " num c;
+      print_result suiv
+
 
 let () =
-  let list = ['a'; 'a'; 'a'; 'b'; 'b'; 'c'] in
+  let str = "aaabbb" in
+  let list = string_to_list str in
   let result = encode list in
-  print_result result
+  print_string "to string: ";
+  print_result result;
+  
+
+  let list2 = ['a'; 'a'; 'a'; 'b'; 'b'; 'c'] in
+  let result2 = encode list2 in
+  print_string "to list: ";
+  print_result result2
