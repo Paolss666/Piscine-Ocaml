0e5be9c25abd571402db0628b8a207bb1e80dba4
Nicola PAOLETTI <npaolett@f6r10s2.clusters.42paris.fr>
2026-03-09 17:41:53 +0100

    done ex03

---
+++ Object-Oriented/ex02/src/dalek.ml
@@ -0,0 +1,31 @@
+
+class dalek =
+  object(self)
+  val name = "Dalek" ^ (string_of_int (Random.int 1000))
+  val mutable htp = 100
+  val mutable shield = true
+  initializer
+    print_endline (name ^ " has been created !")
+  
+  method to_string = name ^ " has " ^ (string_of_int htp) ^ " HP"
+
+
+  method talk =
+      let phrases = [|
+        "Explain! Explain!";
+        "Exterminate! Exterminate!";
+        "I obey!";
+        "You are the Doctor! You are the enemy of the Daleks!"
+      |] in
+      print_endline phrases.(Random.int (Array.length phrases))
+
+  method exerminate (target: People.people) =
+    print_endline (name ^ " exerminate " ^ target#to_string);
+    shield <- false;
+    target#die
+
+  method die = 
+    print_endline "Emergency Temporal Shift!";
+    htp <- 0
+  end
+
---
+++ Object-Oriented/ex02/src/main.ml
@@ -0,0 +1,37 @@
+let () =
+  Random.self_init ();
+
+  (* --- Create characters --- *)
+  let rose    = new People.people "Rose Tyler" in
+  let doctor  = new Doctor.doctor "The Doctor" 900 rose in
+  let dalek   = new Dalek.dalek in
+
+  print_endline "\n========== TO STRING ==========";
+  print_endline doctor#to_string;
+  print_endline rose#to_string;
+  print_endline dalek#to_string;
+
+  print_endline "\n========== TALK ==========";
+  doctor#talk;
+  dalek#talk;
+  dalek#talk;
+
+  print_endline "\n========== SONIC SCREWDRIVER ==========";
+  doctor#use_sonic_screwdriver;
+
+  print_endline "\n========== TRAVEL IN TIME ==========";
+  doctor#travel_in_time 900 1963;
+
+  print_endline "\n========== EXTERMINATE ==========";
+  dalek#exerminate rose;
+
+  print_endline "\n========== DOCTOR TAKES DAMAGE ==========";
+  doctor#take_domage 60;
+  doctor#take_domage 60;  (* triggers regeneration *)
+
+  print_endline "\n========== DALEK DIES ==========";
+  dalek#die;
+
+  print_endline "\n========== FINAL STATES ==========";
+  print_endline doctor#to_string;
+  print_endline dalek#to_string
