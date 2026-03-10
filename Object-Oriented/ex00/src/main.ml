acf434dbcb2b9d53e25773927a90060519427efd
Nicola PAOLETTI <npaolett@f6r10s2.clusters.42paris.fr>
2026-03-10 11:24:35 +0100

    new method

--- Object-Oriented/ex00/src/main.ml
+++ Object-Oriented/ex00/src/main.ml
@@ -2,4 +2,5 @@ let () =
   let people = new People.people "TONY" in
   print_endline (people#to_string);
   print_endline (people#talk);
+  print_endline (people#damage);
   print_endline (people#die)
--- Object-Oriented/ex00/src/people.ml
+++ Object-Oriented/ex00/src/people.ml
@@ -7,4 +7,5 @@ class people (name : string) =
     method to_string = name  ^ " has " ^ (string_of_int htp)
     method talk = "I’m " ^ name ^ "! Do you know the Doctor?"
     method die = "Aaaarghh!"
+    method damage = "Aie it's hurt " ^ (string_of_int (htp - 42))
   end
