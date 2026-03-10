11b816e28d0c16edc2a6b17713ba70b9812f7d7b
Nicola PAOLETTI <npaolett@f6r10s2.clusters.42paris.fr>
2026-03-10 11:12:12 +0100

    mutable age

--- Object-Oriented/ex00/src/people.ml
+++ Object-Oriented/ex00/src/people.ml
@@ -3,7 +3,7 @@ class people (name : string) =
     val name = name
     initializer
       Printf.printf "%s has been created!\n" name
-    val htp : int = 100
+    val mutable htp : int = 100
     method to_string = name  ^ " has " ^ (string_of_int htp)
     method talk = "I’m " ^ name ^ "! Do you know the Doctor?"
     method die = "Aaaarghh!"
