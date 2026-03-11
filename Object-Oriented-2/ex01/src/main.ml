6f646b926cea0003abcf907e00bd08ca1e27d548
Nicola PAOLETTI <npaolett@f6r10s2.clusters.42paris.fr>
2026-03-11 09:11:50 +0100

    add more test

--- Object-Oriented-2/ex01/src/main.ml
+++ Object-Oriented-2/ex01/src/main.ml
@@ -12,4 +12,8 @@ let () =
   Printf.printf "%s\n" n2#to_string;
 
   Printf.printf "Water equals Water: %b\n" (w#equals (w :> Molecule.molecule));
-  Printf.printf "Water equals CO2: %b\n" (w#equals (co2 :> Molecule.molecule))
+  Printf.printf "Water equals CO2: %b\n" (w#equals (co2 :> Molecule.molecule));
+  Printf.printf "Ammonia equals Ammonia: %b\n" (nh3#equals (nh3 :> Molecule.molecule));
+  Printf.printf "Ammonia equals Hydrogen Sulfide: %b\n" (nh3#equals (h2s :> Molecule.molecule));
+  Printf.printf "Nitrogen Gas equals Nitrogen Gas: %b\n" (n2#equals (n2 :> Molecule.molecule));
+  Printf.printf "Nitrogen Gas equals Water: %b\n" (n2#equals (w :> Molecule.molecule))
