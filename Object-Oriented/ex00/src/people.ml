5f1d6fb55c57820864df733fa42d3c2e4f095507
Nicola PAOLETTI <npaolett@f6r9s4.clusters.42paris.fr>
2026-03-12 15:35:36 +0100

    done:

--- Object-Oriented/ex00/src/people.ml
+++ Object-Oriented/ex00/src/people.ml
@@ -1,11 +1,11 @@
 class people (name : string) = 
   object
-    val name = name
+    val _name = name
     initializer
-      Printf.printf "%s has been created!\n" name
-    val mutable hp : int = 100
-    method to_string = name  ^ " has " ^ (string_of_int hp)
-    method talk = "I’m " ^ name ^ "! Do you know the Doctor?"
+      Printf.printf "%s has been created!\n" _name
+    val mutable _hp : int = 100
+    method to_string = _name  ^ " has " ^ (string_of_int _hp)
+    method talk = "I’m " ^ _name ^ "! Do you know the Doctor?"
     method die = "Aaaarghh!"
-    method damage = "Aie it's hurt " ^ (string_of_int (hp - 42))
+    method damage = "Aie it's hurt " ^ (string_of_int (_hp - 42))
   end
