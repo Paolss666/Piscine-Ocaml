49e3607141b2d9f12be10d5d499ec5e5fad7013a
Nicola PAOLETTI <npaolett@f6r9s4.clusters.42paris.fr>
2026-03-12 15:28:13 +0100

    fuck refactor

--- Object-Oriented/ex00/src/people.ml
+++ Object-Oriented/ex00/src/people.ml
@@ -3,9 +3,9 @@ class people (name : string) =
     val name = name
     initializer
       Printf.printf "%s has been created!\n" name
-    val mutable htp : int = 100
-    method to_string = name  ^ " has " ^ (string_of_int htp)
+    val mutable hp : int = 100
+    method to_string = name  ^ " has " ^ (string_of_int hp)
     method talk = "I’m " ^ name ^ "! Do you know the Doctor?"
     method die = "Aaaarghh!"
-    method damage = "Aie it's hurt " ^ (string_of_int (htp - 42))
+    method damage = "Aie it's hurt " ^ (string_of_int (hp - 42))
   end
