da3c6a11d228e463d1e7bd20fd05d1bd92d8d7cb
Nicola PAOLETTI <npaolett@f6r9s4.clusters.42paris.fr>
2026-03-30 10:21:10 +0200

    Hill annotation in main

--- Object-Oriented-2/ex03/src/main.ml
+++ Object-Oriented-2/ex03/src/main.ml
@@ -7,4 +7,10 @@ let () =
   let atoms_start = Reaction.count_atoms [(w, 2); (co2, 1)] in
   List.iter (fun (sym, n) ->
     Printf.printf "  %s: %d\n" sym n
-  ) atoms_start
+  ) (Molecule.hill_sort atoms_start);
+
+  (* 
+      C: 1 (da CO₂) = 1 
+      H: 2×2 (da H₂O×2) = 4
+      O: 2 (da H₂O×2) + 2 (da CO₂) = 4 
+  *)
