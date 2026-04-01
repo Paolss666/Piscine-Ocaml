332b0a9ceb6bedd86d26b397c5ab25c56a48660f
Nicola PAOLETTI <npaolett@f6r12s4.clusters.42paris.fr>
2026-04-01 11:40:37 +0200

    comment ex00

--- Monoids-and-Monads-3/ex00/src/watchtower.ml
+++ Monoids-and-Monads-3/ex00/src/watchtower.ml
@@ -6,12 +6,10 @@ module Watchtower : sig
   val sub  : hour -> hour -> hour
 end = struct
   type hour = int
-
   let zero = 0
 
-  (* Addition wraps around modulo 12 *)
   let add h1 h2 = (h1 + h2) mod 12
 
-  (* Subtraction stays positive modulo 12 *)
   let sub h1 h2 = ((h1 - h2) mod 12 + 12) mod 12
 end
+ (* set of thins pus a rule for combine the thinks and that rules obais at some rules *)
